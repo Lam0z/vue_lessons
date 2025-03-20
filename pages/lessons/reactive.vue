f887f0ad71a2890fc19d622cd2fbcbba90a8e8fe
Nick Efimov <lamoz760@gmail.com>
2025-03-20 10:53:43 +0300

    lesson 2 - Ref/Reactive

--- pages/lessons/reactive.vue
+++ pages/lessons/reactive.vue
@@ -1,11 +1,32 @@
 <script setup>
 const count = ref(0);
-console.log(count.value);
+// console.log(count.value);
+
+const obj = ref({
+    nested: { count: 0 },
+    arr: ["foo", "bar"],
+});
+function mutateDeeply() {
+    // они будут работать, как и ожидалось.
+    obj.value.nested.count++;
+    obj.value.arr.push("baz");
+}
+const o = ref({ count: 0 });
+console.log(o.value.count);
 </script>
 
 <template>
     <MainHeader>Основы реактивности</MainHeader>
 
+    <LPart subtitle="Глубокая реактивность ">
+        <div>
+            <span> {{ obj }} </span>
+        </div>
+        <button class="btn btn-primary" @click="mutateDeeply">
+            Change Obj
+        </button>
+    </LPart>
+    <!--  -->
     <LPart subtitle="Объявление реактивного состояния">
         <span
             v-text="
@@ -16,6 +37,7 @@ const count = ref(0)`
         >
         </span>
     </LPart>
+    <!--  -->
 </template>
 
 <style scoped></style>
