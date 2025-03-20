2498f0be17b235a48cf91799912f044912001bc4
Nick Efimov <lamoz760@gmail.com>
2025-03-20 12:42:31 +0300

    Lesson 3 - Computed

--- layouts/home.vue
+++ layouts/home.vue
@@ -9,6 +9,9 @@
             <li>
                 <nuxt-link to="/lessons/reactive" class=" ">Reactive</nuxt-link>
             </li>
+            <li>
+                <nuxt-link to="/lessons/computed" class=" ">Computed</nuxt-link>
+            </li>
         </ul>
     </header>
     <main>
---
+++ pages/lessons/computed.vue
@@ -0,0 +1,33 @@
+<script setup>
+const author = ref({
+    name: "John Doe",
+    books: [
+        "Vue 2 - Advanced Guide",
+        "Vue 3 - Basic Guide",
+        "Vue 4 - The Mystery",
+    ],
+});
+
+const publishedBooks = computed(() => {
+    return author.value.books.length > 2 ? "Yes" : "No";
+});
+const calculateBooksMessage = () => {
+    return author.value.books.length > 1 ? "Yes" : "No";
+};
+console.log(publishedBooks.value);
+</script>
+
+<template>
+    <MainHeader>Вычисляемые свойства</MainHeader>
+
+    <LPart subtitle="Простой пример ">
+        <div>Есть опубликованные книги:</div>
+        <div>{{ publishedBooks }}</div>
+        <button class="btn btn-primary" @click="author.books.pop()">
+            Cut a book
+        </button>
+    </LPart>
+    <!--  -->
+</template>
+
+<style scoped></style>
